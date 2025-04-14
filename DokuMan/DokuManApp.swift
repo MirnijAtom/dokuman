d4cdb6838468844f37f4e16848797bd9dba185ed
MirnijAtom <alrein1155@gmail.com>
2025-04-14 11:19:04 +0200

    Initial Commit

---
+++ DokuMan/DokuManApp.swift
@@ -0,0 +1,17 @@
+//
+//  DokuManApp.swift
+//  DokuMan
+//
+//  Created by Aleksandrs Bertulis on 14.04.25.
+//
+
+import SwiftUI
+
+@main
+struct DokuManApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DokuManTests/DokuManTests.swift
@@ -0,0 +1,17 @@
+//
+//  DokuManTests.swift
+//  DokuManTests
+//
+//  Created by Aleksandrs Bertulis on 14.04.25.
+//
+
+import Testing
+@testable import DokuMan
+
+struct DokuManTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
