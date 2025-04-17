d315878cd2e4bf712c2aecbc1a65420890cbd852
MirnijAtom <alrein1155@gmail.com>
2025-04-17 12:42:03 +0200

    have categories view

---
+++ DocumentListView.swift
@@ -0,0 +1,18 @@
+//
+//  DocumentListView.swift
+//  DokuMan
+//
+//  Created by Aleksandrs Bertulis on 17.04.25.
+//
+
+import SwiftUI
+
+struct DocumentListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DocumentListView()
+}
