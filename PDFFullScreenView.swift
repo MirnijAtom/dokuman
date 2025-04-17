e8118ea43f577ffd472113be70d1e2375b050b79
MirnijAtom <alrein1155@gmail.com>
2025-04-17 16:30:22 +0200

    Grid and list view introduced, full screen mode is worked on

---
+++ PDFFullScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  PDFFullScreenView.swift
+//  DokuMan
+//
+//  Created by Aleksandrs Bertulis on 17.04.25.
+//
+
+import SwiftUI
+
+struct PDFFullScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PDFFullScreenView()
+}
