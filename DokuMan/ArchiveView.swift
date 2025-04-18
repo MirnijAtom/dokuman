28faf39408f18293e1bd0abf2c6985ce708733a6
MirnijAtom <alrein1155@gmail.com>
2025-04-18 17:51:10 +0200

    Archive maade, needs adjustments

---
+++ DokuMan/ArchiveView.swift
@@ -0,0 +1,18 @@
+//
+//  ArchiveView.swift
+//  DokuMan
+//
+//  Created by Aleksandrs Bertulis on 18.04.25.
+//
+
+import SwiftUI
+
+struct ArchiveView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArchiveView()
+}
