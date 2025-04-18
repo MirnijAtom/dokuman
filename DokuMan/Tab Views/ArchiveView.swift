fdf6d95bded44c5b92b78184f584f3e37a1467d8
MirnijAtom <alrein1155@gmail.com>
2025-04-18 19:04:55 +0200

    The documents are grayed out when archived

--- DokuMan/Tab Views/ArchiveView.swift
+++ DokuMan/Tab Views/ArchiveView.swift
@@ -14,7 +14,6 @@ struct ArchiveView: View {
 
     let columns = [
         GridItem(.flexible()),
-        GridItem(.flexible()),
         GridItem(.flexible())
     ]
 
@@ -22,22 +21,39 @@ struct ArchiveView: View {
     @State private var listView = false
     @State private var selectedDocument: Document? = nil
 
+    
+    // A4 size in points (595 x 842)
+    let a4Size = CGSize(width: 155, height: 218)
+    
     var body: some View {
         NavigationStack {
             ScrollView {
-                LazyVGrid(columns: columns, spacing: 6) {
+                LazyVGrid(columns: columns, spacing: 20) {
                     ForEach(documents) { document in
                         Button(action: {
                             selectedDocument = document
                             fullScreenIsPresented = true
                         }) {
                             VStack(spacing: 8) {
-                                PDFPreview(data: document.versions.first!.fileData)
-                                    .frame(height: 200)
-                                    .cornerRadius(12)
+                                ZStack {
+                                    // A4 sized container
 
+                                    Rectangle()
+                                        .frame(width: a4Size.width - 4, height: a4Size.height - 4)
+                                        .foregroundColor(.gray.opacity(0.2)) // gray background
+                                        .cornerRadius(2)
+                                        .shadow(radius: 2)
+                                    
+                                    PDFPreview(data: document.versions.first!.fileData)
+                                        .scaledToFill() // Scale the PDF to fill the fixed container
+                                        .frame(width: a4Size.width, height: a4Size.height) // Set the fixed size
+                                        .blur(radius: 0.4)
+                                        .opacity(0.6)
+                                        .cornerRadius(2)
+                                }
                                 Text(document.name)
                                     .font(.caption)
+                                    .foregroundColor(.secondary)
                             }
                         }
                         .contextMenu {
