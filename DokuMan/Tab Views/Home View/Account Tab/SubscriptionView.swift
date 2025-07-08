70bfbd744b6f24c5f65df1df5644731482f72682
MirnijAtom <alrein1155@gmail.com>
2025-07-08 18:01:02 +0200

    subscription page done

---
+++ DokuMan/Tab Views/Home View/Account Tab/SubscriptionView.swift
@@ -0,0 +1,18 @@
+//
+//  SubscriptionView.swift
+//  DokuMan
+//
+//  Created by Aleksandrs Bertulis on 08.07.25.
+//
+
+import SwiftUI
+
+struct SubscriptionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SubscriptionView()
+}
